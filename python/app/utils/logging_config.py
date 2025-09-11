39771252c943d97309df91432ea19daeccf838d2
shrsin3 <singhaniamanushree3@gmail.com>
2025-09-11 11:52:25 -0700

    File path update logging config

--- python/app/utils/logging_config.py
+++ python/app/utils/logging_config.py
@@ -1,13 +1,17 @@
 import logging
 from pythonjsonlogger import jsonlogger
 import os
+from pathlib import Path
 
 
 logger = logging.getLogger(__name__)
 
-os.makedirs("./logs", exist_ok=True)
+current_file = Path(__file__).resolve()
+base_dir = current_file.parents[2]
+logs_dir = os.path.join(base_dir, "logs")
+os.makedirs(logs_dir, exist_ok=True)
 
-fileHandler = logging.FileHandler("./logs/app.log")
+fileHandler = logging.FileHandler(os.path.join(logs_dir, "app.log"))
 
 jsonFmt = jsonlogger.JsonFormatter(
     "%(name)s %(asctime)s %(levelname)s %(filename)s %(lineno)s %(process)d %(message)s",
