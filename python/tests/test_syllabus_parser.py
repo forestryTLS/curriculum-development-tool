8c730605d58b0c82a639c7901e7bab707496ce72
shrsin3 <singhaniamanushree3@gmail.com>
2025-09-11 10:24:25 -0700

    Duplicate test removed, path modified

--- python/tests/test_syllabus_parser.py
+++ python/tests/test_syllabus_parser.py
@@ -1,12 +1,13 @@
 import app.syllabus_parser as sp
 import os
+from pathlib import Path
 
 
 def test_syllabi_word_template_with_tabular_assessments():
-    file_path = "tests/data/syllabi/TEST101_2025W1_Word_Tabular_Assessments.docx"
-    ab_path = os.path.abspath(file_path)
+    BASE_DIR = Path(__file__).parent
+    file_path = BASE_DIR /"data"/"syllabi"/"TEST101_2025W1_Word_Tabular_Assessments.docx"
     file_name = "TEST101_2025W1_Word_Tabular_Assessments.docx"
-    course_instance = sp.get_course_from_text_file(ab_path, file_name)
+    course_instance = sp.get_course_from_text_file(file_path, file_name)
     assert course_instance['code'] == 'TEST'
     assert course_instance['number'] == 101
     assert course_instance['term'] == 'W1'
@@ -27,10 +28,10 @@ def test_syllabi_word_template_with_tabular_assessments():
                                               ('Test Individual final Exam', 30)] # Tests the clean_assessment_and_weights function to remove multiple "\n" in assessment names
 
 def test_syllabi_word_template_with_assessments_with_brackets():
-    file_path = "tests/data/syllabi/TEST101_2025W1_Word_Assessments_with_brackets.docx"
-    ab_path = os.path.abspath(file_path)
+    BASE_DIR = Path(__file__).parent
+    file_path = BASE_DIR /"data"/"syllabi"/"TEST101_2025W1_Word_Assessments_with_brackets.docx"
     file_name = "TEST101_2025W1_Word_Assessments_with_brackets.docx"
-    course_instance = sp.get_course_from_text_file(ab_path, file_name)
+    course_instance = sp.get_course_from_text_file(file_path, file_name)
     assert course_instance['code'] == 'TEST'
     assert course_instance['number'] == 101
     assert course_instance['term'] == 'W1'
@@ -49,36 +50,13 @@ def test_syllabi_word_template_with_assessments_with_brackets():
                                               ('Test participation', 3), 
                                               ('Test Midterm', 40), 
                                               ('Test Individual final Exam', 30)]
-    
-def test_syllabi_pdf_template_with_tabular_assessments():
-    file_path = "tests/data/syllabi/TEST101_2025W1_PDF_Tabular_Assessments.pdf"
-    ab_path = os.path.abspath(file_path)
-    file_name = "TEST101_2025W1_PDF_Tabular_Assessments.pdf"
-    course_instance = sp.get_course_from_text_file(ab_path, file_name)
-    assert course_instance['code'] == 'TEST'
-    assert course_instance['number'] == 101
-    assert course_instance['term'] == 'W1'
-    assert course_instance['year'] == 2025
-    assert course_instance['title'] == 'TEST Course Syllabus' 
-    assert course_instance['level'] == 'Undergraduate'
-    assert course_instance['description'] == 'This course provides a comprehensive introduction to [subject area], focusing on the key \nconcepts, issues, and practices that shape the field. Students will explore the historical \nbackground, current trends, and future directions of [subject area], engaging with a variety of \nperspectives and resources. The course blends lectures, discussions, and applied activities to help \nstudents understand how ideas in this domain are developed, debated, and implemented. \nThroughout the term, students will gain exposure to foundational theories as well as \ncontemporary approaches, gaining insight into the ways [subject area] influences academic \nresearch, industry practice, and everyday life. The course also offers opportunities to work with \nreal-world examples and case studies, encouraging students to make connections between \nabstract concepts and practical applications. \nThis description outlines the scope and nature of the course, providing students with a clear sense \nof the themes and topics that will be covered. Specific learning objectives, assessment criteria, \nand expected outcomes are detailed in separate sections of the syllabus.'
-    assert course_instance['goals'] == ['Explain the core concepts, theories, and terminology related to [subject area].', 
-                                        'Apply appropriate methods, tools, or frameworks to analyze problems and develop solutions within [subject area].', 
-                                        'Evaluate and critique information or arguments using evidence-based reasoning.', 
-                                        'Communicate ideas and findings effectively in written, oral, or visual formats appropriate to the field.', 
-                                        'Explore the connections between theoretical knowledge and real-world practice.', 
-                                        'Integrate knowledge gained in class with real-world or interdisciplinary contexts.']
-    assert course_instance['assessments'] == [('Test Assignments', 27), 
-                                              ('Test participation', 3), 
-                                              ('Test Midterm', 40), 
-                                              ('Test Individual final Exam', 30)]
 
 
 def test_syllabi_pdf_template_with_tabular_assessments():
-    file_path = "tests/data/syllabi/TEST101_2025W1_PDF_Tabular_Assessments.pdf"
-    ab_path = os.path.abspath(file_path)
+    BASE_DIR = Path(__file__).parent
+    file_path = BASE_DIR /"data"/"syllabi"/"TEST101_2025W1_PDF_Tabular_Assessments.pdf"
     file_name = "TEST101_2025W1_PDF_Tabular_Assessments.pdf"
-    course_instance = sp.get_course_from_text_file(ab_path, file_name)
+    course_instance = sp.get_course_from_text_file(file_path, file_name)
     assert course_instance['code'] == 'TEST'
     assert course_instance['number'] == 101
     assert course_instance['term'] == 'W1'
@@ -99,10 +77,10 @@ def test_syllabi_pdf_template_with_tabular_assessments():
 
 
 def test_syllabi_pdf_template_CLOs_without_bullets():
-    file_path = "tests/data/syllabi/TEST101_2025W1_PDF_Without_Bullet_CLOs.pdf"
-    ab_path = os.path.abspath(file_path)
+    BASE_DIR = Path(__file__).parent
+    file_path = BASE_DIR /"data"/"syllabi"/"TEST101_2025W1_PDF_Without_Bullet_CLOs.pdf"
     file_name = "TEST101_2025W1_PDF_Without_Bullet_CLOs.pdf"
-    course_instance = sp.get_course_from_text_file(ab_path, file_name)
+    course_instance = sp.get_course_from_text_file(file_path, file_name)
     assert course_instance['code'] == 'TEST'
     assert course_instance['number'] == 101
     assert course_instance['term'] == 'W1'
@@ -123,10 +101,10 @@ def test_syllabi_pdf_template_CLOs_without_bullets():
     
 
 def test_syllabi_pdf_template_without_course_title_table():
-    file_path = "tests/data/syllabi/TEST101_2025W1_Without_Course_Title_table.pdf"
-    ab_path = os.path.abspath(file_path)
+    BASE_DIR = Path(__file__).parent
+    file_path = BASE_DIR /"data"/"syllabi"/"TEST101_2025W1_Without_Course_Title_table.pdf"
     file_name = "TEST101_2025W1_Without_Course_Title_table.pdf"
-    course_instance = sp.get_course_from_text_file(ab_path, file_name)
+    course_instance = sp.get_course_from_text_file(file_path, file_name)
     assert course_instance['code'] == 'TEST'
     assert course_instance['number'] == 101
     assert course_instance['term'] == 'W1'
@@ -146,10 +124,10 @@ def test_syllabi_pdf_template_without_course_title_table():
                                               ('Test Individual final Exam', 30)]
     
 def test_syllabi_file_name_without_course_code_number_and_term():
-    file_path = "tests/data/syllabi/FileNameWithoutCode&Term.pdf"
-    ab_path = os.path.abspath(file_path)
+    BASE_DIR = Path(__file__).parent
+    file_path = BASE_DIR /"data"/"syllabi"/"FileNameWithoutCode&Term.pdf"
     file_name = "FileNameWithoutCode&Term.pdf"
-    course_instance = sp.get_course_from_text_file(ab_path, file_name)
+    course_instance = sp.get_course_from_text_file(file_path, file_name)
     assert course_instance['code'] == 'TEST'
     assert course_instance['number'] == 101
     assert course_instance['term'] == 'W2'
@@ -170,10 +148,10 @@ def test_syllabi_file_name_without_course_code_number_and_term():
 
 
 def test_syllabi_file_name_without_course_code_number_term_and_multiple_codes_in_file():
-    file_path = "tests/data/syllabi/FilenameWithoutCode&MultipleCodeInFile.pdf"
-    ab_path = os.path.abspath(file_path)
+    BASE_DIR = Path(__file__).parent
+    file_path = BASE_DIR /"data"/"syllabi"/"FilenameWithoutCode&MultipleCodeInFile.pdf"
     file_name = "FilenameWithoutCode&MultipleCodeInFile.pdf"
-    course_instance = sp.get_course_from_text_file(ab_path, file_name)
+    course_instance = sp.get_course_from_text_file(file_path, file_name)
     assert course_instance['code'] == 'TEST'
     assert course_instance['number'] == 101
     assert course_instance['term'] == 'W2'
@@ -194,10 +172,10 @@ def test_syllabi_file_name_without_course_code_number_term_and_multiple_codes_in
     
     
 def test_syllabi_different_layout():
-    file_path = "tests/data/syllabi/DifferentLayoutSyllabus.pdf"
-    ab_path = os.path.abspath(file_path)
+    BASE_DIR = Path(__file__).parent
+    file_path = BASE_DIR /"data"/"syllabi"/"DifferentLayoutSyllabus.pdf"
     file_name = "DifferentLayoutSyllabus.pdf"
-    course_instance = sp.get_course_from_text_file(ab_path, file_name)
+    course_instance = sp.get_course_from_text_file(file_path, file_name)
     assert course_instance['code'] == 'TEST'
     assert course_instance['number'] == 101
     assert course_instance['term'] == 'W2'
@@ -218,10 +196,10 @@ def test_syllabi_different_layout():
     
     
 def test_syllabi_different_layout_assessments_percentage_seperated():
-    file_path = "tests/data/syllabi/DifferentLayoutSyllabusWithComponentsPrecentageSeperated.pdf"
-    ab_path = os.path.abspath(file_path)
+    BASE_DIR = Path(__file__).parent
+    file_path = BASE_DIR /"data"/"syllabi"/"DifferentLayoutSyllabusWithComponentsPrecentageSeperated.pdf"
     file_name = "DifferentLayoutSyllabusWithComponentsPrecentageSeperated.pdf"
-    course_instance = sp.get_course_from_text_file(ab_path, file_name)
+    course_instance = sp.get_course_from_text_file(file_path, file_name)
     assert course_instance['code'] == 'TEST'
     assert course_instance['number'] == 101
     assert course_instance['term'] == 'W2'
@@ -242,10 +220,10 @@ def test_syllabi_different_layout_assessments_percentage_seperated():
     
 
 def test_syllabi_description_with_additional_section():
-    file_path = "tests/data/syllabi/TEST101_Syllabus_Description_With_Additional_Section.pdf"
-    ab_path = os.path.abspath(file_path)
+    BASE_DIR = Path(__file__).parent
+    file_path = BASE_DIR /"data"/"syllabi"/"TEST101_Syllabus_Description_With_Additional_Section.pdf"
     file_name = "TEST101_Syllabus_Description_With_Additional_Section.pdf"
-    course_instance = sp.get_course_from_text_file(ab_path, file_name)
+    course_instance = sp.get_course_from_text_file(file_path, file_name)
     assert course_instance['code'] == 'TEST'
     assert course_instance['number'] == 101
     assert course_instance['term'] == 'W2'
@@ -266,10 +244,10 @@ def test_syllabi_description_with_additional_section():
 
 
 def test_syllabi_CLOs_with_prefixed():
-    file_path = "tests/data/syllabi/TEST101_2025W1_CLOs_with_prefixes.pdf"
-    ab_path = os.path.abspath(file_path)
+    BASE_DIR = Path(__file__).parent
+    file_path = BASE_DIR /"data"/"syllabi"/"TEST101_2025W1_CLOs_with_prefixes.pdf"
     file_name = "TEST101_2025W1_CLOs_with_prefixes.pdf"
-    course_instance = sp.get_course_from_text_file(ab_path, file_name)
+    course_instance = sp.get_course_from_text_file(file_path, file_name)
     assert course_instance['code'] == 'TEST'
     assert course_instance['number'] == 101
     assert course_instance['term'] == 'W1'
