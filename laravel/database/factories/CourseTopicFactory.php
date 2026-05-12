2524d3f586bd2ad8d31787cbeea9dffc13ee20c8
NEOGDAWG <sodhikabir2005@gmail.com>
2026-05-12 14:17:05 -0700

    Created course topic and course material factories for future testing

---
+++ laravel/database/factories/CourseTopicFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CourseTopic>
+ */
+class CourseTopicFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'course_id' => Course::factory(),
+            'topic' => $this->faker->sentence(5),
+            'description' => $this->faler->optional()->paragraph(),
+            'position' => $this->faker->numberBetween(0, 100),
+        ];
+    }
+}
