78fe6c89722478fea41ac75a45e76902b0cd3739
NEOGDAWG <sodhikabir2005@gmail.com>
2026-06-12 11:22:33 -0700

    fix shared maxlength validation scope

--- laravel/resources/views/courses/wizard/header.blade.php
+++ laravel/resources/views/courses/wizard/header.blade.php
@@ -721,9 +721,8 @@
         location.reload();
     });
 
-    $(document).ready(function () {
 	//This method is used to make sure that the proper amount of characters are entered so it doesn't exceed the max character limits
-    function validateMaxlength(e){
+    function validateMaxlength(){
         //Whitespaces are counted as 1 but character wise are 2 (\n).
         var MAX_LENGTH = event.target.getAttribute("maxlength");
         var currentLength = event.target.value.length;
@@ -734,5 +733,4 @@
             event.target.value = event.target.value.substr(0,MAX_LENGTH-(whiteSpace)+1);
         }
     }
-    });
 </script>
